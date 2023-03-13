3b13f8fdca8314a1a171fd88f5d294b6f976efbe
Yeonseo-Jo <jys073190@gmail.com>
2023-03-13 20:57:44 +0900

    INT: firebase 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import firebaseApp from "./firebase";
 import "./App.css";
 
 function App() {
+  //firebase 설치 확인용
+  console.log(firebaseApp);
   return <div className="App">App</div>;
 }
 
