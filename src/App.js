ce77d483b32577596dd0188b24a3f4b3cb1d6224
Yeonseo-Jo <jys073190@gmail.com>
2023-03-15 11:45:01 +0900

    FIX: 로그인 관련 리다이렉트 수정, 에러 처리

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import { app, auth } from "./firebase";
+import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import Home from "./pages/Home";
 import SignIn from "./pages/Login/SignIn";
 import SignUp from "./pages/Login/SignUp";
 
 function App() {
-  //firebase import 확인용
-  // console.log("app", app);
-  // console.log("auth", auth);
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
+      console.log(user);
       if (user) {
         setIsLoggedIn(user);
       } else {
