c750993ce0c0091b33b1d7491e0cf21a1e35becf
Yeonseo-Jo <jys073190@gmail.com>
2023-03-21 11:04:40 +0900

    FTR: stt 녹음 페이지-병원 입력, 카테고리 선택 구현

--- src/pages/Record.js
+++ src/pages/Record.js
@@ -1,4 +1,16 @@
+import Category from "../components/Category";
+
 function Record() {
-  return <h1> Record </h1>;
+  return (
+    <>
+      <h1> 진료 내용 기록하기 </h1>
+      <input
+        name="hospital"
+        type="text"
+        placeholder="병원 이름을 입력해 주세요."
+      />
+      <Category />
+    </>
+  );
 }
 export default Record;
