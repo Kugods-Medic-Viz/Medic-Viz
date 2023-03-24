cbe54dc2d8405f17913ed3be6f1836e939356075
Yeonseo-Jo <jys073190@gmail.com>
2023-03-24 13:58:43 +0900

    FTR: CSS 기본 틀 (styled-component)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import { theme, size } from "./styles/theme";
+import { GlobalStyle } from "./styles/globalStyles";
+import { ThemeProvider } from "styled-components";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme} size={size}>
+      <BrowserRouter basename="/">
+        <App />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export const size = {
+  mobile: "414px",
+  tablet: "820px",
+  laptop: "1200px",
+  desktop: "1800px",
+};
+
+export const theme = {
+  subColor: "#005DEA",
+  bgColor: "#DEEBFF",
+  fontColor: "#3D3D3D",
+  light: "#C8C8C8",
+  light2: "#A1A1A1",
+  arrowGradiant:
+    "linear-gradient(180deg, rgba(0, 93, 234, 0.25) 0%, #005DEA 100%)",
+  red: "#FF0000",
+};
