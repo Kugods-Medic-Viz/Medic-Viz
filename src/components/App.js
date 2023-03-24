74c0be662f06d28a8a6140dbaba83343d506a09c
Yeonseo-Jo <jys073190@gmail.com>
2023-03-24 13:58:43 +0900

    FTR: CSS 기본 틀 (styled-component)

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Home from "../pages/Home";
 import SignIn from "../pages/Login/SignIn";
 import SignUp from "../pages/Login/SignUp";
 import Record from "../pages/Record";
+import styled from "styled-components";
 
 function App() {
   const [init, setInit] = useState(false);
@@ -24,7 +25,7 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Wrap>
       <Link to="/">Home</Link>
       <Routes>
         <Route path="/" element={<Home isLoggendIn={isLoggedIn} />}></Route>
@@ -32,8 +33,19 @@ function App() {
         <Route path="/signin" element={<SignIn />}></Route>
         <Route path="/recording" element={<Record />}></Route>
       </Routes>
-    </>
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  min-height: calc(var(--vh, 1vh) * 100);
+  * {
+    max-width: 480px;
+  }
+`;
 
 export default App;
