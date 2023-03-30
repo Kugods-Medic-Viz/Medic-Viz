568045c188eef5f7a612bbd27e7b2af70f1e601e
hwangmina <hmina2001@naver.com>
2023-03-30 19:36:32 +0900

    FTR: 홈화면, 네비게이션 css1차

---
+++ src/styles/Home.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const HomeWrap = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LogoImg = styled.button`
+  width: 79px;
+  height: 99px;
+  background: url("img/Home/LogoImg.png");
+  background-size: cover;
+  border: none;
+  margin: 80px 0px 0px 70px;
+`;
+
+export const Logo = styled.button`
+  width: 121px;
+  height: 99px;
+  background: url("img/Home/LogoText.png");
+  background-size: cover;
+  border: none;
+  margin: 20px 0px 100px 70px;
+`;
+
+export const BtnWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-left: 55px;
+  padding-right: 55px;
+  margin-bottom: 100px;
+`;
+
+export const RecordBtn = styled.button`
+  width: 180px;
+  height: 189px;
+  background: url("img/Home/RecordBtn.png");
+  background-size: cover;
+  border: none;
+`;
+
+export const AlgorithmBtn = styled.button`
+  width: 180px;
+  height: 189px;
+  background: url("img/Home/AlgorithmBtn.png");
+  background-size: cover;
+  border: none;
+`;
+
+
